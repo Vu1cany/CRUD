22097f768a68157390018747cd112a8f0bff15b8
Alexey Nikolaev <leshanikolaev2004@yandex.ru>
2023-02-24 12:03:29 +0300

    Fixed(final version)

--- src/Solution.java
+++ src/Solution.java
@@ -20,94 +20,88 @@ public class Solution {
         SimpleDateFormat dateWriteFormat = new SimpleDateFormat("dd-MMM-yyyy", Locale.ENGLISH);
 
         try {
+            String name;
+            Sex sex;
+            String sexStr;
+            Date birthday;
+            int index;
+            String birthdayStr;
+
+
             switch (args[0]){
                 case "-c":
-                    if (args[1] != null && args[2] != null && args[3] != null){
-                        String name = args[1];
+                    
+                    name = args[1];
 
-                        Sex sex;
-                        if (args[2].equals("м")){
-                            sex = Sex.MALE;
-                        } else if (args[2].equals("ж")){
-                            sex = Sex.FEMALE;
-                        } else {
-                            throw new InvalidParameterException("Пол введен неправильно");
-                        }
-
-                        Date birthday = dateReadFormat.parse(args[3]);
-
-                        if (sex == Sex.MALE) {
-                            allPeople.add(Person.createMale(name, birthday));
-                        } else {
-                            allPeople.add(Person.createFemale(name, birthday));
-                        }
-                        System.out.println(allPeople.size() - 1);
-
+                    if (args[2].equals("м")){
+                        sex = Sex.MALE;
+                    } else if (args[2].equals("ж")){
+                        sex = Sex.FEMALE;
                     } else {
-                        throw new NullPointerException("Введены не все аргументы");
+                        throw new InvalidParameterException("Пол введен неправильно");
                     }
+
+                    birthday = dateReadFormat.parse(args[3]);
+
+                    if (sex == Sex.MALE) {
+                        allPeople.add(Person.createMale(name, birthday));
+                    } else {
+                        allPeople.add(Person.createFemale(name, birthday));
+                    }
+                    System.out.println(allPeople.size() - 1);
+
                     break;
                 case "-r":
-                    if (args[1] != null) {
-                        int index = Integer.parseInt(args[1]);
-                        Person person = allPeople.get(index);
 
-                        String sex;
-                        if (person.getSex() == Sex.MALE){
-                            sex = "м";
-                        } else {
-                            sex = "ж";
-                        }
+                    index = Integer.parseInt(args[1]);
+                    Person person = allPeople.get(index);
 
-                        String birthday = dateWriteFormat.format(person.getBirthDate());
-                        System.out.println(person.getName() + " " + sex + " " + birthday);
 
+                    if (person.getSex() == Sex.MALE){
+                        sexStr = "м";
                     } else {
-                        throw new NullPointerException("Введены не все аргументы");
+                        sexStr = "ж";
                     }
+
+                    birthdayStr = dateWriteFormat.format(person.getBirthDate());
+                    System.out.println(person.getName() + " " + sexStr + " " + birthdayStr);
+
                     break;
                 case "-u":
-                    if (args[1] != null && args[2] != null && args[3] != null && args[4] != null){
-                        int index = Integer.parseInt(args[1]);
-                        String name = args[2];
 
-                        Sex sex;
-                        if (args[3].equals("м")){
-                            sex = Sex.MALE;
-                        } else if (args[3].equals("ж")){
-                            sex = Sex.FEMALE;
-                        } else {
-                            throw new InvalidParameterException("Пол введен неправильно");
-                        }
+                    index = Integer.parseInt(args[1]);
+                    name = args[2];
 
-                        Date birthday = dateReadFormat.parse(args[4]);
-
-                        if (sex == Sex.FEMALE) {
-                            allPeople.set(index, Person.createFemale(name, birthday));
-                        } else if (sex == Sex.MALE){
-                            allPeople.set(index, Person.createMale(name, birthday));
-                        }
-
+                    if (args[3].equals("м")){
+                        sex = Sex.MALE;
+                    } else if (args[3].equals("ж")){
+                        sex = Sex.FEMALE;
                     } else {
-                        throw new NullPointerException("Введены не все аргументы");
+                        throw new InvalidParameterException("Пол введен неправильно");
                     }
+
+                    birthday = dateReadFormat.parse(args[4]);
+
+                    if (sex == Sex.FEMALE) {
+                        allPeople.set(index, Person.createFemale(name, birthday));
+                    } else if (sex == Sex.MALE){
+                        allPeople.set(index, Person.createMale(name, birthday));
+                    }
+
                     break;
                 case "-d":
-                    if (args[1] != null) {
-                        int index = Integer.parseInt(args[1]);
-                        Person person = allPeople.get(index);
-                        person.setSex(null);
-                        person.setName(null);
-                        person.setBirthDate(null);
 
-                    } else {
-                        throw new NullPointerException("Введены не все аргументы");
-                    }
+                    index = Integer.parseInt(args[1]);
+                    person = allPeople.get(index);
+                    person.setSex(null);
+                    person.setName(null);
+                    person.setBirthDate(null);
+
                     break;
-                default:
-
             }
 
+        } catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Недостаточное количество аргументов программы");
         } catch (Exception e){
             System.out.println(e.getMessage());
         }
